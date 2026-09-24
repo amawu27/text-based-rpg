77de866e4a270627bf1e9aec5bca724fcc5907b6
Paul <christianpauldiaz27@gmail.com>
2026-09-24 22:45:52 +0800

    Added easter egg

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const locations =
     {
         name: "kill monster",
         "button text": ["Go to Town Square", "Go to Town Square", "Go to Town Square"],
-        "button functions": [goTown, goTown, goTown],
+        "button functions": [goTown, goTown, easterEgg],
         text: "The monster screams Arg! as it dies. You gain experience points and find gold."
     },
 
@@ -90,6 +90,13 @@ const locations =
         "button text": ["Replay?", "Replay?", "Replay?"],
         "button functions": [restart, restart, restart],
         text: "You defeated the dragon! YOU WIN THE GAME!"
+    },
+
+    {
+        name: "easter egg",
+        "button text": ["2", "8", "Go to Town Square?"],
+        "button functions": [pickTwo, pickEight, goTown],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ];
 
@@ -315,4 +322,50 @@ function getMonsterAttackValue(level)
 function isMonsterHit()
 {
     return Math.random() > 0.2 || health < 20;
+}
+
+function easterEgg()
+{
+    update(locations[7]);
+}
+
+function pickTwo()
+{
+    pick(2);
+}
+
+function pickEight()
+{
+    pick(8);
+}
+
+function pick(guess)
+{
+    let numbers = [];
+    while(numbers.length < 10)
+    {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+
+    for(let i = 0; i < 10; i++)
+    {
+        text.innerText += numbers[i] + "\n";   
+    }
+
+    if(numbers.indexOf(guess) !== -1)
+    {
+        gold+= 20;
+        goldText.innerText = gold;
+        text.innerText += "Right! You win 20 gold!";
+    }
+    else
+    {
+        health -= 10;
+        healthText.innerText = health;
+        text.innerText += "Wrong! You lose 10 health!"
+        if(health <= 0)
+            lose();
+    }
 }
